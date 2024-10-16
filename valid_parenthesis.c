83afde5aa46cb384235c18ddecf459b12024d1a4
Prajwalks05 <prajwalks.cs24@bmsce.ac.in>
2024-10-16 19:18:41 +0530

    Update valid_parenthesis.c

--- valid_parenthesis.c
+++ valid_parenthesis.c
@@ -1,29 +1,21 @@
-//Leet code problem #20;
+// Leet code problem #20;
 #include <string.h>
 bool isValid(char* s) {
     char opn_stk[10];
-    char cls_stk[10];
-    for (int i = 0; i < strlen(s); i++) 
-    {
-        int j=0;
-        char c = s[0];
-        if (c == '(' || c == '{' || c == '[')
-        {
-            opn_stk[j] = c;
+    int top = s[0];
+    for (int i = 0; i < strlen(s); i++) {
+        char c = s[i];
+
+        if (c == '(' || c == '{' || c == '[') {
+            opn_stk[i] = c;
+            top++;
         }
-        
-        if (c == ')' || c == '}' || c == ']') 
-        {
-            cls_stk[j] = c;
-        }
-        if(opn_stk[j]==cls_stk[j])
-        {
-            return true;   
-        }
-        else
-        {
-            return false;
+        for (int j = 0; j < strlen(s); j++) {
+            if (c== s[i]) {
+                return true;
+            }
         }
     }
-    return 0;
+
+    return false;
 }
