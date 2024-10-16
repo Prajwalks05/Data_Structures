07638d42cf69fe96c3f5792b6fbb29ed266bdc61
Prajwalks05 <prajwalks.cs24@bmsce.ac.in>
2024-10-16 19:26:07 +0530

    Update valid_parenthesis.c

--- valid_parenthesis.c
+++ valid_parenthesis.c
@@ -1,21 +1,32 @@
 // Leet code problem #20;
 #include <string.h>
+#include <stdbool.h>
+
 bool isValid(char* s) {
     char opn_stk[10];
-    int top = s[0];
+    int top = 0;
+
     for (int i = 0; i < strlen(s); i++) {
         char c = s[i];
 
         if (c == '(' || c == '{' || c == '[') {
-            opn_stk[i] = c;
-            top++;
-        }
-        for (int j = 0; j < strlen(s); j++) {
-            if (c== s[i]) {
-                return true;
+            opn_stk[top++] = c; 
+        } 
+        else {
+            if (top == 0) {
+                return false; 
+            }
+            char last = opn_stk[top - 1]; 
+            if ((c == ')' && last == '(') || 
+                (c == '}' && last == '{') || 
+                (c == ']' && last == '[')) {
+                top--;
+            } else {
+                return false;
             }
         }
     }
 
-    return false;
+    return top == 0; // If stack is empty, all brackets matched
 }
+
