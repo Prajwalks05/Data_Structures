81ccd8046d50b1233fe186ff47c7cecdf6c25a42
Prajwalks05 <prajwalks.cs24@bmsce.ac.in>
2024-11-12 23:28:33 +0530

    Update queue_with_ll.c

--- queue_with_ll.c
+++ queue_with_ll.c
@@ -1,64 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node
+typedef struct node
 {
     int data;
-    struct Node *next;
+    struct node *next;
 } node;
 
-void push(node **front, node **rear, int val)
+node *front = NULL, *rear = NULL;
+
+void enqueue(int val)
 {
-    node *newnode = malloc(sizeof(node));
-    newnode->data = val;
-    newnode->next = NULL;
-    if (*front == NULL && *rear == NULL)
+    node *newNode = malloc(sizeof(node));
+    if (newNode == NULL)
     {
-        *front = *rear = newnode;
+        printf("Memory allocation failed\n");
+        return;
+    }
+    newNode->data = val;
+    newNode->next = NULL;
+
+    if (front == NULL && rear == NULL)
+    {
+        front = rear = newNode;
     }
     else
     {
-        (*rear)->next = newnode;
-        *rear = newnode;
+        rear->next = newNode;
+        rear = newNode;
     }
 }
 
-void dequeue(node **front, node **rear)
+void dequeue()
 {
-    if (*front)
+    node *temp;
+
+    if (front == NULL)
     {
-        node *temp = *front;
-        *front = (*front)->next;
-        if (*front == NULL)
+        printf("Queue is Empty. Unable to perform dequeue\n");
+    }
+    else
+    {
+        temp = front;
+        front = front->next;
+
+        if (front == NULL)
         {
-            *rear = NULL;
+            rear = NULL;
         }
+        printf("Dequeued Element:%d\n", temp->data);
         free(temp);
     }
 }
 
-void display(node **front)
+void display_queue()
 {
-    node *temp = *front;
+    node *temp = front;
+    if (temp == NULL)
+    {
+        printf("Queue is empty.\n");
+        return;
+    }
     while (temp)
     {
-        printf("%d->", temp->data);
+        printf("%d -> ", temp->data);
         temp = temp->next;
     }
     printf("NULL\n");
 }
 
-int main(void)
+int main()
 {
-    node *front = NULL;
-    node *rear = NULL;
-    push(&front, &rear, 10);
-    push(&front, &rear, 20);
-    push(&front, &rear, 30);
-    push(&front, &rear, 40);
-    display(&front);
-    dequeue(&front, &rear);
-    dequeue(&front, &rear);
-    display(&front);
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    enqueue(40);
+    enqueue(50);
+    printf("Queue: ");
+    display();
+    printf("Dequeue\n");
+    dequeue();
+    dequeue();
+    display();
+
     return 0;
 }
